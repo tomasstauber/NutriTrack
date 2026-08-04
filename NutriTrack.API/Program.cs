c396a7ec3c79efc61b5d5c4b5659f2e7f67c3cd2
30605656 <dlamerataalumno@itec-elmolino.edu.ar>
2026-08-04 19:29:33 -0300

    feat: desactivacion y reactivacion de animal CU4/CU5

--- NutriTrack.API/Program.cs
+++ NutriTrack.API/Program.cs
@@ -15,6 +15,7 @@ builder.Services.AddScoped<PlanAlimenticioRepository>();
 builder.Services.AddScoped<AltaAnimalRepository>();
 builder.Services.AddScoped<ConsultaFichaIndividualAnimalRepository>();
 builder.Services.AddScoped<IngredienteRepository>();
+builder.Services.AddScoped<DesactivacionReactivacionAnimalRepository>();
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
