0ba4e2551a1b5c809ce55b9a34d91054a99fccd0
gala mailen tapia <galu@192.168.1.10>
2026-08-24 23:28:15 -0300

    feat: crear usuario CU22

---
+++ NutriTrack.Core/Entities/Usuario.cs
@@ -0,0 +1,19 @@
+namespace NutriTrack.Core.Entities
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Correo { get; set; }
+
+        public string NombreUsuario { get; set; }
+
+        public string Rol { get; set; }
+
+        public string Contrasenia { get; set; }
+
+        public bool Activo { get; set; } = true;
+    }
+}
